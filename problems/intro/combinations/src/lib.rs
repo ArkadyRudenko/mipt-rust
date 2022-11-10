605a8e60434d42bf3f3334c9e2cdc85b7fca8b70
ArkadyRudenko <rudenko.arkashaa@gmail.com>
2022-11-10 15:15:00 +0600

    intro/min-queue

--- problems/intro/combinations/src/lib.rs
+++ problems/intro/combinations/src/lib.rs
@@ -2,6 +2,7 @@
 
 pub fn go_combinations(offset: usize, k: usize, result: &mut Vec<Vec<i32>>, src: &[i32], combination: &mut Vec<i32>) {
     if k == 0 {
+        // combination is ready
         result.push(combination.clone());
         return;
     }
@@ -11,12 +12,11 @@ pub fn go_combinations(offset: usize, k: usize, result: &mut Vec<Vec<i32>>, src:
         go_combinations(i + 1, k - 1, result, src, combination);
         combination.pop();
     }
-
 }
 
 
 pub fn combinations(arr: &[i32], k: usize) -> Vec<Vec<i32>> {
-    if k == 0 || arr.len() == 0{
+    if k == 0 || arr.len() == 0 {
         return vec![vec![]];
     }
 
