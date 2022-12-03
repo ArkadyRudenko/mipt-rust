f5dcc403967c13975d779e8570359799545a82d4
ArkadyRudenko <rudenko.arkashaa@gmail.com>
2022-12-03 22:29:30 +0600

    std-collections/comm

--- problems/std-collections/comm/src/main.rs
+++ problems/std-collections/comm/src/main.rs
@@ -1,8 +1,36 @@
 #![forbid(unsafe_code)]
 
-// TODO: your code goes here.
+use std::{fs::File, io::BufRead, io::BufReader, collections::HashSet};
+use std::io::{Write};
 
 fn main() {
-    // TODO: your code goes here.
-    unimplemented!()
+    let args = std::env::args().collect::<Vec<String>>();
+
+    let file1 = File::open(args[1].clone()).unwrap();
+    let file2 = File::open(args[2].clone()).unwrap();
+
+    let reader1 = BufReader::new(file1);
+    let reader2 = BufReader::new(file2);
+
+    let mut lines1 = HashSet::new();
+    let mut lines2 = HashSet::new();
+
+    for line1 in reader1.lines() {
+        match line1 {
+            Ok(line) => lines1.insert(line),
+            _ => false,
+        };
+    }
+    for line2 in reader2.lines() {
+        match line2 {
+            Ok(line) => lines2.insert(line),
+            _ => false,
+        };
+    }
+
+    for line in lines1 {
+        if lines2.contains(&*line) {
+            println!("{}", line);
+        }
+    }
 }
