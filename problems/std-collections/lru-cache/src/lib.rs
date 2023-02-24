ebe8eaba40ad5e342fc0544213a749f2bcf25b44
arkadii_rudenko <rudenko.arkashaa@gmail.com>
2023-02-25 01:17:15 +0600

    [+] trust and lru

--- problems/std-collections/lru-cache/src/lib.rs
+++ problems/std-collections/lru-cache/src/lib.rs
@@ -1,26 +1,53 @@
 #![forbid(unsafe_code)]
 
-use std::collections::{BTreeMap, HashMap};
+use std::collections::{HashMap, VecDeque};
 use std::hash::Hash;
 
 #[derive(Debug)]
 pub struct LRUCache<K, V> {
-    // TODO: your code goes here.
+    table: HashMap<K, V>,
+    key_queue: VecDeque<K>,
+    cap: usize,
 }
 
 impl<K: Clone + Hash + Ord, V> LRUCache<K, V> {
     pub fn new(capacity: usize) -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        if capacity == 0 {
+            panic!("cap is zero");
+        }
+        Self {
+            table: HashMap::with_capacity(capacity),
+            key_queue: VecDeque::with_capacity(capacity),
+            cap: capacity,
+        }
     }
 
     pub fn get(&mut self, key: &K) -> Option<&V> {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.table.get(key)
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        // TODO: your code goes here.
-        unimplemented!()
+        // println!("len = {}, cap = {}", self.table.len(), self.cap);
+        if !self.table.contains_key(&key) && !self.is_full() {
+            // println!("insert {}", self.table.capacity());
+            if self.key_queue.len() == 0 {
+                self.key_queue.push_back(key.clone());
+            }
+            self.table.insert(key.clone(), value);
+        } else if !self.table.contains_key(&key) && self.is_full() {
+            // println!("2 if:");
+            let min_key = self.key_queue.pop_back();
+            self.table.remove(&min_key.unwrap());
+            self.table.insert(key, value);
+        } else if self.table.contains_key(&key) {
+            return self.table.insert(key.clone(), value);
+        }
+        // println!("-----");
+        None
     }
+
+    fn is_full(&self) -> bool {
+        self.table.len() == self.cap
+    }
+
 }
--- problems/traits/trust/src/lib.rs
+++ problems/traits/trust/src/lib.rs
@@ -10,28 +10,57 @@ pub enum RoundOutcome {
     BothCheated,
 }
 
+pub trait Player {
+    fn is_put_coin(&self) -> bool;
+    fn update_behaviour(&mut self, is_cooperative_of_adversary: bool);
+}
+
 pub struct Game {
-    // TODO: your code goes here.
+    left_player: Box<dyn Player>,
+    right_player: Box<dyn Player>,
+    left_coins: i32,
+    right_score: i32,
 }
 
 impl Game {
-    // pub fn new(left: Box<???>, right: Box<???>) -> Self {
-    // TODO: your code goes here.
-    // }
+    pub fn new(left: Box<dyn Player>, right: Box<dyn Player>) -> Self {
+        Self {
+            left_player: left,
+            right_player: right,
+            left_coins: 0,
+            right_score: 0,
+        }
+    }
 
     pub fn left_score(&self) -> i32 {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.left_coins
     }
 
     pub fn right_score(&self) -> i32 {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.right_score
     }
 
     pub fn play_round(&mut self) -> RoundOutcome {
-        // TODO: your code goes here.
-        unimplemented!()
+        let left = self.left_player.is_put_coin();
+        let right = self.right_player.is_put_coin();
+
+        self.right_player.update_behaviour(left);
+        self.left_player.update_behaviour(right);
+        return if left && right {
+            self.left_coins += 2;
+            self.right_score += 2;
+            RoundOutcome::BothCooperated
+        } else if !left && !right {
+            RoundOutcome::BothCheated
+        } else if !left && right {
+            self.left_coins += 3;
+            self.right_score -= 1;
+            RoundOutcome::LeftCheated
+        } else {
+            self.left_coins -= 1;
+            self.right_score += 3;
+            RoundOutcome::RightCheated
+        };
     }
 }
 
@@ -40,31 +69,143 @@ impl Game {
 #[derive(Default)]
 pub struct CheatingAgent {}
 
-// TODO: your code goes here.
+impl Player for CheatingAgent {
+    fn is_put_coin(&self) -> bool {
+        false
+    }
+
+    fn update_behaviour(&mut self, is_cooperative_of_adversary: bool) {}
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Default)]
 pub struct CooperatingAgent {}
 
-// TODO: your code goes here.
+impl Player for CooperatingAgent {
+    fn is_put_coin(&self) -> bool {
+        true
+    }
+
+    fn update_behaviour(&mut self, is_cooperative_of_adversary: bool) {}
+}
+////////////////////////////////////////////////////////////////////////////////
+
+pub struct GrudgerAgent {
+    is_cooperative: bool,
+}
+
+impl Default for GrudgerAgent {
+    fn default() -> Self {
+        Self {
+            is_cooperative: true,
+        }
+    }
+}
+
+impl Player for GrudgerAgent {
+    fn is_put_coin(&self) -> bool {
+        self.is_cooperative
+    }
+
+    fn update_behaviour(&mut self, is_cooperative_of_adversary: bool) {
+        if !is_cooperative_of_adversary {
+            self.is_cooperative = false;
+        }
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
-pub struct GrudgerAgent {}
+pub struct CopycatAgent {
+    is_cooperative: bool,
+}
 
-// TODO: your code goes here.
+impl Default for CopycatAgent {
+    fn default() -> Self {
+        Self {
+            is_cooperative: true,
+        }
+    }
+}
 
-////////////////////////////////////////////////////////////////////////////////
+impl Player for CopycatAgent {
+    fn is_put_coin(&self) -> bool {
+        self.is_cooperative
+    }
 
-pub struct CopycatAgent {}
-
-// TODO: your code goes here.
+    fn update_behaviour(&mut self, is_cooperative_of_adversary: bool) {
+        self.is_cooperative = is_cooperative_of_adversary
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
 pub struct DetectiveAgent {
-    // TODO: your code goes here.
+    is_cooperative: bool,
+    iter: usize,
+    adver_is_cheat: bool,
+    is_ready: bool,
 }
 
-// TODO: your code goes here.
+impl Default for DetectiveAgent {
+    fn default() -> Self {
+        Self {
+            is_cooperative: true,
+            iter: 0,
+            adver_is_cheat: false,
+            is_ready: false,
+        }
+    }
+}
+
+struct Behavior {
+    steps: Vec<bool>,
+    step_index: usize,
+    is_fixed_behavior: bool,
+}
+
+impl Behavior {
+    fn new() -> Self {
+        Self {
+            steps: vec![true, false, true, true],
+            step_index: 0,
+            is_fixed_behavior: true,
+        }
+    }
+
+    fn get_step(&self) -> bool {
+        self.steps[self.step_index]
+    }
+
+    fn update_behaviour(&mut self, adver_step: bool) {
+        self.step_index += 1;
+    }
+}
+
+impl Player for DetectiveAgent {
+    fn is_put_coin(&self) -> bool {
+        self.is_cooperative
+    }
+
+    fn update_behaviour(&mut self, is_cooperative_of_adversary: bool) {
+        self.iter += 1;
+        if !is_cooperative_of_adversary && !self.is_ready {
+            self.is_ready = true;
+            self.adver_is_cheat = true;
+        }
+        if self.iter < 4 {
+            if self.iter == 1 {
+                self.is_cooperative = false;
+            } else if self.iter == 2 {
+                self.is_cooperative = true;
+            }
+        } else {
+            if self.adver_is_cheat {
+                self.is_cooperative = is_cooperative_of_adversary;
+            } else {
+                self.is_cooperative = false;
+            }
+        }
+    }
+}
